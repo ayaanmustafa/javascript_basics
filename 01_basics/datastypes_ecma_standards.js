a6256341bce981b028e1209112650f60aae4f16d
ayaanmustafa <69399872+ayaanmustafa@users.noreply.github.com>
2025-05-23 06:32:32 +0000

    datatypes

---
+++ 01_basics/datastypes_ecma_standards.js
@@ -0,0 +1,18 @@
+//"use strict" // not needed but it explicitly mentions that we are using the new js
+
+//alert(6+7) // doesn't work while using nodejs, only works when used with a web browser wrapper
+
+/*
+number => max 2^53
+bigint => greater than number
+string => "", ''
+boolean => True, False
+null => standalone value
+undefined => well undefined
+object
+*/
+
+console.log(typeof null) // gives object
+console.log(typeof undefined) // gives undefined only as it is a defined type in js
+
+// official standards of js: https://262.ecma-international.org/
