ddabafe922f4d5aa5f551e5d2ff6b6f4d16fc7f6
ayaanmustafa <69399872+ayaanmustafa@users.noreply.github.com>
2025-05-23 06:05:45 +0000

    comments and let

---
+++ 01_basics/javascipts_var_let_consts.js
@@ -0,0 +1,12 @@
+const accountId = 22
+let customer_email = "smth@gmail.com"
+var age = 44
+let id;  //undefined variable 
+
+// ctrl slash for comments
+
+/* big comments
+Never use var due to its inability to handle scope
+
+*/
+console.table([accountId,customer_email,age])
