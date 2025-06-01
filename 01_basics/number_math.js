6718295b674180b6c6d81e667686d3ccbebb8e23
ayaanmustafa <69399872+ayaanmustafa@users.noreply.github.com>
2025-06-01 06:41:09 +0000

    math

--- 01_basics/number_math.js
+++ 01_basics/number_math.js
@@ -3,3 +3,19 @@ console.log(con.toPrecision(5))
 console.log(con.toFixed(5))
 console.log(con.toLocaleString("en-IN"))
 console.log(con.toLocaleString())
+
+let e = Number.EPSILON
+console.log(e.toPrecision(4))
+
+/////////   MATHS   ////////
+
+console.log(Math.random())
+console.log(Math.round(4.5))
+
+const min = 1
+const max = 6
+
+console.log(Math.floor((Math.random())*(max-min+1)+min))
+
+//max-min range of value
+//max-min+1 avoids zero case
